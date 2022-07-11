e368577d01e594913d6c1c1106c79e733c94731a
Radko Ksenia <ksradko16@yandex.ru>
2022-07-11 12:50:50 +0300

    Init project

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import Counter from "./components/counter";
+import React from "react";
+
+const App = () => {
+    return <Counter />
+}
+export default App;
---
+++ src/components/counter.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+const Counter = () => {
+
+    const [count, setCount] = useState(0);
+    
+
+    const formatCount = () => {
+        return count === 0 || count < 0? "empty": count;
+    };
+
+    const getBageClasses = () => {
+        let classes = "badge m-2 ";
+        classes += count === 0 || count < 0? "bg-warning": "bg-primary";
+        return classes
+    };
+
+    const handleIncrement = () => {
+        setCount((prevState) => prevState + 1)
+        console.log(count)
+    }
+
+    const handleDecrement = () => {
+        setCount((prevState) => prevState - 1)
+        console.log(count)
+    }
+
+    return (
+        <>
+            <span className= {getBageClasses()}>{formatCount()}</span>
+            <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>+</button>
+            <button className="btn btn-primary btn-sm" onClick={handleDecrement}>-</button>
+        </>
+    );
+}
+
+export default Counter;
